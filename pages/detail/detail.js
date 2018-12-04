ea54742f7e420fd98c2fae74905218c66e2872e6
zhouwenchao <875037306@qq.com>
2018-12-04 21:08:20 +0800

    修改列表页面，使其符合要求

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,66 +1,32 @@
-// pages/detail/detail.js
+
+import fetch from '../../utils/fetch.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    id:-1,
+    detail:{}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(e){
+    console.log(e)
+    this.setData({
+      id : e.id
+    })
+    this.loadDetail()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  loadDetail(){
+    fetch({
+      url:`/shops/${this.data.id}`
+    }).then(res => {
+      this.setData({
+        detail:res.data
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  previewImage(e){
+    console.log("图片预览",e)
   }
+
 })
